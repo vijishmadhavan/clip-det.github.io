28ce12123350b579bcca7f991f33368b1c6a9501
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2022-10-18 14:26:18 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import { NsfwSpy } from './@nsfwspy/browser';
-const img = document.getElementById("img");
-const nsfwSpy = new NsfwSpy();
-await nsfwSpy.load();
-const result = await nsfwSpy.classifyImage(img);
-console.log(result);
